74267dd458812ba892a4cdce3088b6ea8311855f
Bernhard Bücherl <bernhard.buecherl@gmail.com>
2015-03-05 22:20:44 +0100

    initial commit v0.0.1

---
+++ README.md
@@ -0,0 +1,42 @@
+# clidejs/ee.c
+
+simple event emitter implementation for c/c++ inspired by
+[nodejs events.EventEmitter][1].
+
+## Usage
+
+`#include "ee.h"` in your project and build with `ee.c` as source.
+
+## Documentation
+
+coming soon
+
+## Test
+
+see `ee-test.c`. Run with `g++ ee-test.c -o ee && ./ee`.
+
+## License: MIT
+
+Copyright (c) 2015 [Bernhard Bücherl](https://github.com/bbuecherl)
+
+-------------------------------------------------------------------
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+    [1]: http://nodejs.org/api/events.html#events_class_events_eventemitter
---
+++ ee-test.c
@@ -0,0 +1,48 @@
+/* ee - eventemitter for c - ee-test.c
+ * https://github.com/clidejs/ee.c
+ *
+ * Copyright Bernhard Bücherl <bernhard.buecherl@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "ee.h"
+#include "ee.c"
+#include <iostream>
+
+#define EVENT_NAME 1
+
+void callback(void *ptr) {
+  std::cout << ptr << std::endl;
+}
+int main() {
+  ee_emitter_t *ee =(ee_emitter_t *)malloc(sizeof(ee_emitter_t));
+  ee_init(ee);
+
+  ee_on(ee, EVENT_NAME, callback);
+
+  ee_emit(ee, EVENT_NAME, 0);
+
+  ee_off(ee, EVENT_NAME, callback);
+
+  ee_emit(ee, EVENT_NAME, 0);
+
+  ee_destroy(ee);
+  return 0;
+}
